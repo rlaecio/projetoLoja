2df02a6220054f05f07d74e3062a74562065403d
Roque Laecio dos Santos <rlaecio@hotmail.com>
2020-06-22 11:54:45 +0100

    Update cadastro.c

--- s_loja/cadastro.c
+++ s_loja/cadastro.c
@@ -4,36 +4,34 @@
 int main()
 {
 
-    printf("------------------------------ \n");
-    printf("       Sistema Loja            \n");
-    printf("------------------------------ \n");
+    printf("------------------------------------ \n");
+    printf("             Sistema Loja            \n");
+    printf("------------------------------------ \n");
     printf("\n");
 
     /* Array ppara o sistema principal */
-    
-    /* Variaveis do sistema de vendas */
-    char nome_produto[30], nome_prod[100];
-    double quantidade[100], valor_unitario[100], valor2, desconto, total, totalDesconto = 0, percentual[100];
-    int categoria, vet_posicao = 0, continuar, continua;
+    struct listaProdutos
+    {
+        char nome_prod[100];
+        float quantidade;
+        float val_unitario;
+        float percentual;
+    };
 
-    do
+    /* Variaveis do sistema de vendas */
+    int posicaoVetor = 0;
+    struct listaProdutos cadastro;
+
+    do // Inicia o laço ate que se peça para cancelar
     {
         printf("Digite o nome do produto: ");
-        scanf("%s", &nomeProduto);
+        fflush(stdin);
+        fgets(cadastro.nome_prod, 100, stdin);
 
         printf("\n");
 
         printf("########### ENTRE COM A CATEGORIA ########### \n \n1 - Informatica \n2 - Jogos  \n3 - Eletronicos \n \n");
         scanf("%i", &categoria);
-
-        printf("Digite a quantidade:  ");
-        scanf("%f", &quantidade[vet_posicao]);
-
-        printf("Digite o valor unitario:  ");
-        scanf("%f", &valor_unitario[vet_posicao]);
-
-        &nome_prod[vet_posicao] = nome_produto; 
-        valor2 = valor_unitario[vet_posicao] * quantidade[vet_posicao];
         switch (categoria)
         {
 
@@ -56,23 +54,33 @@ int main()
             desconto = 0.05;
             break;
         }
-        &percentual[vet_posicao] = valor2 * desconto;
-        total = valor2 - percentual[vet_posicao];
 
-       
+        printf("Digite a quantidade:  ");
+        scanf("%f", &quantidade[posicaoVetor]);
+
+        printf("Digite o valor unitario:  ");
+        scanf("%f", &valor_unitario[posicaoVetor]);
+
+        &nome_prod[posicaoVetor] = nome_produto;
+        valor2 = valor_unitario[posicaoVetor] * quantidade[posicaoVetor];
+
+        &percentual[posicaoVetor] = valor2 * desconto;
+        total = valor2 - percentual[posicaoVetor];
+
         printf("1  - Continuar cadastrando.  2 - Sair  :");
         scanf("%d", &continuar);
-        if(continuar == 1)
+        if (continuar == 1)
         {
             &continua = 1;
             break;
-        } else {
-            &continua = 0;
-            &vet_posicao = vet_posicao + 1;
         }
-        
-    } while(continua == 0);
-    
+        else
+        {
+            &continua = 0;
+            posicaoVetor++;
+        }
+
+    } while (continua == 0);
 
     printf("\n");
     printf("-------------------------------------------- \n");
